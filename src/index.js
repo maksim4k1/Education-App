ca24ce1112e9c3d3bc25ea852e0e8743ee8b79bb
Maksim Bazhenov <maksim4k1@gmail.com>
2021-11-06 00:51:14 +0600

    added styles for Error404 page, edited styles for Main page and added gh-pages library

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './App';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import store from './redux';
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <React.StrictMode>
         <App />
       </React.StrictMode>
-    </BrowserRouter>
+    </HashRouter>
   </Provider>,
   document.getElementById('root')
 );
--- src/pages/errors/Error404.jsx
+++ src/pages/errors/Error404.jsx
@@ -1,10 +1,34 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+
+const Container = styled.div`
+  padding-top: 150px;
+  display: flex;
+  flex-flow: column;
+`;
+const ErrorTitle = styled.h1`
+  margin: 0 0 20px;
+  font-size: 60px;
+  font-weight: 700;
+`;
+const ErrorDeskription = styled.p`
+  font-size: 20px;
+`;
+const Link = styled(NavLink)`
+  color: var(--color-blue);
+  font-weight: 700;
+  &:hover{
+    text-decoration: underline;
+  }
+`;
 
 function Error404 () {
   return(
-    <div>
-      
-    </div>
+    <Container className="container">
+      <ErrorTitle>Error 404</ErrorTitle>
+      <ErrorDeskription>Page not found. <Link to="/">Go to main page</Link></ErrorDeskription>
+    </Container>
   );
 }
 
